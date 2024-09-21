fe4be9e106a7c97381df823474871025ef361d82
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-09-21 22:10:41 +0530

    Signup Page for Users

--- frontend/user-frontend/src/App.tsx
+++ frontend/user-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { SigninPage } from "./pages/signin-page"
 import { HomePage } from "./pages/home-page"
+import { SignupPage } from "./pages/signup-page"
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
          <Route path="/signin" element={<SigninPage />} />
+         <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </BrowserRouter>
     </>
